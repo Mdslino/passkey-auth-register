d4d500cfef787c61be085b3a705c14ec321afcbe
Marcelo Lino <mdslino@gmail.com>
2024-11-13 22:56:20 -0300

    Fix build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import wasm from "@rollup/plugin-wasm";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', 'nuxt-shiki'],
   shiki: { defaultTheme: 'nord' },
+  devServer: { port: 4000 },
+  vite: {
+    plugins: [wasm()],
+  },
 })
